9296b289b7eb525568f4d4d2b339e80c42263d2f
TonyFenton <tony.fenton3@gmail.com>
2018-07-08 13:55:24 +0200

    Fix a migration

--- src/Migrations/Version20180531165011.php
+++ src/Migrations/Version20180531165011.php
@@ -15,7 +15,9 @@ class Version20180531165011 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE template DROP FOREIGN KEY FK_97601F83162CB942');
         $this->addSql('ALTER TABLE template CHANGE folder_id folder_id INT NOT NULL');
+        $this->addSql('ALTER TABLE template ADD CONSTRAINT FK_97601F83162CB942 FOREIGN KEY (folder_id) REFERENCES folder (id)');
     }
 
     public function down(Schema $schema)
